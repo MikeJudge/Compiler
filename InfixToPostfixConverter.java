91d3f874276b95d45dc0bba6bd40dc75b2c479ce
MikeJudge <thejudgement223@gmail.com>
2015-02-28 18:01:08 -0500

    fully functioning postfix converter and evaluator

--- InfixToPostfixConverter.java
+++ InfixToPostfixConverter.java
@@ -1,7 +1,7 @@
 import java.util.Stack;
 
 public class InfixToPostfixConverter {
-	String expression;
+	private String expression;
 
 	public InfixToPostfixConverter(String expression) {
 		this.expression = expression;
@@ -19,7 +19,7 @@ public class InfixToPostfixConverter {
 		while (!stack.isEmpty()) {
 			c = infix.charAt(i);
 			if (Character.isDigit(c)) {
-				//loop supports numbers larger than 9
+				//supports numbers larger than 9
 				while (Character.isDigit(c)) {
 					postfix.append(c);
 					c = infix.charAt(++i);
@@ -31,7 +31,7 @@ public class InfixToPostfixConverter {
 				stack.push(c);
 			else if (isOperator(c)) {
 				while (!stack.isEmpty()) {
-					if (isOperator(stack.peek()) && precedence(c, stack.peek()))
+					if (isOperator(stack.peek()) && precedence(stack.peek(), c))
 						postfix.append(stack.pop() + " ");
 					else {
 						stack.push(c);
@@ -53,23 +53,22 @@ public class InfixToPostfixConverter {
 	private boolean precedence(char operator1, char operator2) {
 		int op1 = -1;
 		int op2 = -1;
-		String [] precedenceRules = {"^", "*?%", "+-"};
+		String [] precedenceRules = {"+ -", "* ? % /", "^"};
 		for (int i = 0; i < precedenceRules.length; i++) {
 			if (precedenceRules[i].indexOf(operator1) != -1)
-				op1 = precedenceRules[i].indexOf(operator1);
+				op1 = i;
 
 			if (precedenceRules[i].indexOf(operator2) != -1)
-				op2 = precedenceRules[i].indexOf(operator2);
+				op2 = i;
 		}
-
-		if (op1 > op2)
+		if (op1 >= op2)
 			return true;
 		return false;
 
 	}
 
 
-	private boolean isOperator(char c) {
+	private static boolean isOperator(char c) {
 		if (c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '^')
 			return true;
 
@@ -77,7 +76,8 @@ public class InfixToPostfixConverter {
 	}
 
 	public static void main(String [] args) {
-		InfixToPostfixConverter test = new InfixToPostfixConverter("(625+20)* 5 -8/ 4");
+		//625 20 + 5 * 8 4 / - 
+		InfixToPostfixConverter test = new InfixToPostfixConverter("(-625+20)* 5 -8/ 4");
 		System.out.println(test.getPostfix());
 	}
 }
---
+++ PostfixEvaluator.java
@@ -0,0 +1,65 @@
+import java.util.Stack;
+
+public class PostfixEvaluator {
+	private String expression;
+
+	public PostfixEvaluator(String postFixExpression) {
+		expression = postFixExpression;
+	}
+
+	public int evaluate() {
+		Stack<Integer> stack = new Stack<>();
+		char c;
+
+		String [] expressionArr = expression.split(" ");
+		String string;
+		int x, y;
+
+		for (int i = 0; i < expressionArr.length; i++) {
+			string = expressionArr[i];
+			//could be a one digit number, a variable, or a operator
+			if (string.length() == 1)
+				//operator: take the top two items evaluate them using the operand, and push the result to the stack
+				if (isOperator(string.charAt(0))) {
+					char operator = string.charAt(0);
+					int result;
+					x = stack.pop();
+					if (stack.empty())
+						y = 0;
+					else 
+						y = stack.pop();
+
+					switch (operator) {
+						case '*': stack.push(y * x); break;
+						case '/': stack.push(y / x); break;
+						case '+': stack.push(y + x); break;
+						case '-': stack.push(y - x); break;
+						case '%': stack.push(y % x); break;
+					}
+
+
+				} else { //must be a number
+					stack.push(Integer.parseInt(string));
+				}
+			else //must be a number
+				stack.push(Integer.parseInt(string))	;
+		}
+		return stack.pop();
+	}
+
+
+	private static boolean isOperator(char c) {
+		if (c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '^')
+			return true;
+
+		return false;
+	}
+
+	public static void main(String [] args) {
+		PostfixEvaluator test = new PostfixEvaluator("625 - 20 + 5 * 8 4 / - ");
+		System.out.println(test.evaluate());
+	}
+
+
+
+}
